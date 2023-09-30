67931a994bce59b51f68d27aa14fd93f1c7570bd
ytaousi <yasssir.taous@gmail.com>
2023-09-30 04:31:01 +0100

    .. setting tor configs

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ RUN apt update && apt upgrade -y && apt install -y vim nginx wget tor curl ssh
 COPY ./confs/nginx.conf /etc/nginx/sites-enabled/default
 COPY ./confs/index.html /var/www/html/index.html
 COPY ./confs/torrc /etc/tor/torrc
-# COPY ./confs/sshd_config /etc/ssh/sshd_config
+COPY ./confs/sshd_config /etc/ssh/sshd_config
+RUN echo 'root:Docker!' | chpasswd
 
 COPY ./confs/setupServices.sh /setupServices.sh
 RUN chmod +x /setupServices.sh
--- confs/setupServices.sh
+++ confs/setupServices.sh
@@ -5,11 +5,11 @@ sed -i '0,/#HiddenServicePort 80 127.0.0.1:80/s//HiddenServicePort 80 127.0.0.1:
 
 sed -i "s|#Port 22|Port 22|g" /etc/ssh/sshd_config
 sed -i "s|#ListenAddress 0.0.0.0|ListenAddress 0.0.0.0|g" /etc/ssh/sshd_config
-sed -i "s|#PermitRootLogin prohibit-password|PermitRootLogin no|g"
-# sed -i "s|#PasswordAuthentication yes|PasswordAuthentication yes|g" /etc/ssh/sshd_config
-# sed -i "s|#PermitEmptyPasswords no|PermitEmptyPasswords no|g" /etc/ssh/sshd_config
+sed -i "s|#PermitRootLogin prohibit-password|PermitRootLogin yes|g" /etc/ssh/sshd_config
+sed -i "s|#PasswordAuthentication yes|PasswordAuthentication yes|g" /etc/ssh/sshd_config
+sed -i "s|#PermitEmptyPasswords no|PermitEmptyPasswords no|g" /etc/ssh/sshd_config
 
+service ssh start
 mkdir -p /run/sshd
-/usr/sbin/sshd -D
 nginx -g 'daemon off;'
 
